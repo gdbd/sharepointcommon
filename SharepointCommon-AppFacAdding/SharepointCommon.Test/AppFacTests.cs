840480154520a064ec5a5370c11db3d9173c440d
devi_ous_cp <SND\devi_ous_cp>
2013-03-18 05:46:54 +0000

    fix Docs project for vs2012, start implementing AppFac

---
+++ SharepointCommon-AppFacAdding/SharepointCommon.Test/AppFacTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SharepointCommon.Test
+{
+    [TestFixture]
+    public class AppFacTests
+    {
+        [Test]
+        public void GetCurrentTest()
+        {
+            var app = TestAppFac.Open("asd");
+        }
+    }
+
+    public class TestAppFac : AppFac<TestApp>
+    {
+    }
+
+    public class TestApp : AppBase
+    {
+        public TestApp(IQueryWeb queryWeb) : base(queryWeb)
+        {
+        }
+    }
+}
---
+++ SharepointCommon-AppFacAdding/SharepointCommon/AppFac.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SharepointCommon
+{
+    public class AppFac<T> where T : AppBase
+    {
+        public static T GetCurrent()
+        {
+            return (T)Activator.CreateInstance(typeof(T), WebFactory.CurrentContext());
+        }
+
+        public static T Open(Guid siteId, Guid webId)
+        {
+            return (T)Activator.CreateInstance(typeof(T), WebFactory.Open(siteId, webId));
+        }
+
+        public static T Open(string webUrl)
+        {
+            return (T)Activator.CreateInstance(typeof(T), WebFactory.Open(webUrl));
+        }
+    }
+
+    public class AppBase
+    {
+        public AppBase(IQueryWeb queryWeb)
+        {
+            QueryWeb = queryWeb;
+        }
+
+        protected IQueryWeb QueryWeb { get; set; }
+    }
+}
