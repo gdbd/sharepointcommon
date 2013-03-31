97e778eeb779d24049866fd640364141b59a0718
devi_ous <5655858+gdbd@users.noreply.github.com>
2013-03-31 19:57:24 +0000

    change ListAttribute.Id to string

--- SharepointCommon.Test/TestApp.cs
+++ SharepointCommon.Test/TestApp.cs
@@ -1,7 +1,13 @@
-﻿namespace SharepointCommon.Test
+﻿using System;
+using SharepointCommon.Attributes;
+
+namespace SharepointCommon.Test
 {
     public class TestApp : AppBase<TestApp>
     {
+        [List(Id = "8A083287-CAEF-4DFA-8246-E8236676F5A1")]
+        public IQueryList<Item> Test { get; set; }
+
         public void SetListThatMustThrows()
         {
             var list = UserInfoList;
