86adb290cf69ce4ed660e5406c025b3e7621efeb
devi_ous_cp <SND\devi_ous_cp>
2013-04-10 07:54:17 +0000

    AppBase "not IQueryList" not needed to mark [NotMapped], allow mark IQueryList property as [NotMapped]

--- SharepointCommon/SharepointCommon/Common/Interceptors/AppBaseAccessInterceptor.cs
+++ SharepointCommon/SharepointCommon/Common/Interceptors/AppBaseAccessInterceptor.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using Castle.DynamicProxy;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Utilities;
