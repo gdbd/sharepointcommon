249ee46b5b7901e920940195483d9e7b1e7649b1
devi_ous_cp <SND\devi_ous_cp>
2016-08-09 07:15:01 +0000

    fix nuget package for shp2013

--- SharepointCommon-v3.0/NuGet/Pack.bat
+++ SharepointCommon-v3.0/NuGet/Pack.bat
@@ -2,10 +2,10 @@ xcopy ..\BUILD\14\SharepointCommon.dll SharepointCommon\lib\net35\ /Y
 xcopy ..\BUILD\14\SharepointCommon.xml SharepointCommon\lib\net35\ /Y
 																 
 xcopy ..\BUILD\15\SharepointCommon.dll SharepointCommon\lib\net45\ /Y
-xcopy ..\BUILD\15\SharepointCommon.xml SharepointCommon\lib\net45\ /Y
+xcopy ..\BUILD\14\SharepointCommon.xml SharepointCommon\lib\net45\ /Y
 																 
 xcopy ..\BUILD\16\SharepointCommon.dll SharepointCommon\lib\net46\ /Y
-xcopy ..\BUILD\16\SharepointCommon.xml SharepointCommon\lib\net46\ /Y
+xcopy ..\BUILD\14\SharepointCommon.xml SharepointCommon\lib\net46\ /Y
 
 
 nuget pack SharepointCommon\SharepointCommon.nuspec
