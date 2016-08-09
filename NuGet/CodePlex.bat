291ef99236b43275d9f883e122b87c20b18fdfe2
devi_ous <5655858+gdbd@users.noreply.github.com>
2016-08-09 07:15:01 +0000

    fix nuget package for shp2013

--- NuGet/CodePlex.bat
+++ NuGet/CodePlex.bat
@@ -1,4 +1,4 @@
-7z.exe a -tzip SharepointCommon_v3.0.4_2010_Binary.zip sharepointcommon\lib\net35\sharepointcommon.dll sharepointcommon\lib\net35\sharepointcommon.xml
-7z.exe a -tzip SharepointCommon_v3.0.4_2013_Binary.zip sharepointcommon\lib\net45\sharepointcommon.dll sharepointcommon\lib\net45\sharepointcommon.xml
-7z.exe a -tzip SharepointCommon_v3.0.4_2016_Binary.zip sharepointcommon\lib\net46\sharepointcommon.dll sharepointcommon\lib\net46\sharepointcommon.xml
+7z.exe a -tzip SharepointCommon_v3.0.5_2010_Binary.zip sharepointcommon\lib\net35\sharepointcommon.dll sharepointcommon\lib\net35\sharepointcommon.xml
+7z.exe a -tzip SharepointCommon_v3.0.5_2013_Binary.zip sharepointcommon\lib\net45\sharepointcommon.dll sharepointcommon\lib\net45\sharepointcommon.xml
+7z.exe a -tzip SharepointCommon_v3.0.5_2016_Binary.zip sharepointcommon\lib\net46\sharepointcommon.dll sharepointcommon\lib\net46\sharepointcommon.xml
 pause
